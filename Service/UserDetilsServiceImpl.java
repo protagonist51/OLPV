6573e2db341a5c432f51c471702c0cae755bc754
yogesh51 <yogeshyvb@gmail.com>
2015-10-22 18:42:16 +0530

    Create UserDetilsServiceImpl.java

---
+++ Service/UserDetilsServiceImpl.java
@@ -0,0 +1,57 @@
+package org.itc.service;
+
+
+import java.util.List;
+
+import org.itc.dao.UserDetailsDao;
+import org.itc.model.User;
+import org.itc.model.UserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired  
+	 private UserDetailsDao userDetailsDao;  
+	   
+	@Transactional
+	 public UserDetails addUserDetails(UserDetails userDetails) throws DataIntegrityViolationException  {  
+	  userDetailsDao.addUserDetails(userDetails);
+	  return userDetailsDao.addUserDetails(userDetails);  
+	 
+	} 
+	
+	@Transactional
+	public List<UserDetails> showUserDetails(int userId){
+		  return userDetailsDao.showUserDetails(userId);
+		 }
+	
+	@Transactional
+	public List<UserDetails> getAllDetails() {
+		
+		return userDetailsDao.usersList();
+	}
+	
+	@Transactional
+	public UserDetails editUserdetails(int id){
+		
+		return userDetailsDao.editUserdetails(id);
+	}
+	
+	@Transactional
+	public UserDetails checkforId(User user){
+		
+		return userDetailsDao.checkforId(user);
+	}
+	
+	public UserDetails recordforid(int id){
+		return userDetailsDao.recordforid(id);
+		
+	}
+	
+		
+}
