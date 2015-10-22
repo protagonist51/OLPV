2d45100af5f76e523c4c314455598c493c1b0c0d
yogesh51 <yogeshyvb@gmail.com>
2015-10-22 18:28:13 +0530

    Create viewDetailsController.js

---
+++ js/viewDetailsController.js
@@ -0,0 +1,43 @@
+app.controller('viewDetailsController',[ '$scope', '$http', '$location', '$routeParams',  function($scope, $http, $location, $routeParams)
+                                          
+    {
+		
+				$scope.userId = $routeParams.userId;
+		
+			
+			
+				$http.post('viewuserdetails', $scope.userId ).success(function(response)
+						{
+			
+						
+						$scope.detailsforuser = response;
+			
+			
+						}).error(function(error)
+								
+								{
+								alert(error);
+								});
+				
+				
+						$scope.viewdetails =function(id)
+						
+						{
+							$location.url('/userwelcome/'+id);
+						};
+						
+						
+						
+						$scope.Logout = function()
+						{	
+						
+						$http.post('logout', $scope.userId ).success(function(response)	
+									{
+										
+										$location.url('/homepage');
+									});
+							
+						}
+						
+						
+	}]);
