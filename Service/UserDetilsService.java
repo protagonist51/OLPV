1304a7fbb74a4b29e0815266cf23b15b709790bc
yogesh51 <yogeshyvb@gmail.com>
2015-10-22 18:41:53 +0530

    Create UserDetilsService.java

---
+++ Service/UserDetilsService.java
@@ -0,0 +1,26 @@
+package org.itc.service;
+
+
+import java.util.List;
+
+
+import org.itc.model.User;
+import org.itc.model.UserDetails;
+import org.springframework.dao.DataIntegrityViolationException;
+
+
+
+
+public interface UserDetailsService {
+
+	public UserDetails addUserDetails(UserDetails userDetails) throws DataIntegrityViolationException;
+	public List<UserDetails> showUserDetails(int userId);
+	public List<UserDetails> getAllDetails();
+	public UserDetails editUserdetails(int id);
+	public UserDetails recordforid(int id);
+	public UserDetails checkforId(User user);
+
+	
+	
+}
+
