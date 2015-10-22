955dd9c68205f1b766204a3f82e349f8322b027a
yogesh51 <yogeshyvb@gmail.com>
2015-10-22 18:43:00 +0530

    Create UserServiceImpl.java

---
+++ Service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package org.itc.service;
+
+import java.sql.SQLException;
+
+import org.itc.dao.UserDao;
+import org.itc.model.Role;
+import org.itc.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service
+
+public class UserServiceImpl implements UserService {
+	
+	@Autowired  
+	 private UserDao userDao;  
+	
+	 @Transactional
+	 public void addUser(User user) {  
+	  userDao.addUser(user);  
+	 } 
+	
+	
+	    
+	public User findUser(String userName, String password, Role role) throws SQLException
+	    {
+	        return userDao.findUser(userName, password, role);
+	    }
+	
+	public void LogoutUser(int userId){
+		 userDao.LogoutUser(userId);
+	}
+
+	
+
+}
